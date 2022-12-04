c3458507518f6c0744235e4260465a2a5a0492da
Marcin Koch <marcin.koch@tacton.com>
2022-12-04 06:15:32 +0100

    day04

---
+++ day04/main01.js
@@ -0,0 +1,37 @@
+const R = require('ramda');
+const U = require('./utils');
+
+U.runWrapper(parse, run, {
+    hideRaw: true,
+    hideParsed: false
+});
+
+// --------------------------------------------
+
+function parse(lines) {
+    return lines.map(x => x.split(',').map(y => y.split('-').map(Number)));
+}
+
+// --------------------------------------------
+
+function run(data) {
+
+    // U.log('Hello');
+
+    var count = 0;
+
+    data.forEach(p => {
+        const [x1, x2] = p[0];
+        const [y1, y2] = p[1];
+
+        // U.log(x1, x2, y1, y2);
+
+        if ((y1 >= x1 && y2 <= x2) || (x1 >= y1 && x2 <= y2)) {
+            count++;
+        }
+    });
+
+
+    return count;
+}
+
