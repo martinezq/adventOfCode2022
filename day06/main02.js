bb87b6853c947c1be77c5f050e702113fed06c1e
Marcin Koch <marcin.koch@tacton.com>
2022-12-06 06:11:28 +0100

    day06

---
+++ day06/main02.js
@@ -0,0 +1,35 @@
+const R = require('ramda');
+const U = require('./utils');
+
+U.runWrapper(parse, run, {
+    hideRaw: true,
+    hideParsed: false
+});
+
+// --------------------------------------------
+
+function parse(lines) {
+    return lines[0].split('');
+}
+
+// --------------------------------------------
+
+function run(data) {
+
+    // U.log('Hello');
+
+    let counter = 0;
+    let result = 0;
+
+    for (let i=0; i<data.length-4; i++) {
+        const list = R.take(14, R.drop(i, data));
+        if (R.uniq(list).length === 14) {
+            counter++;
+            result = i + 1;
+            break;
+        }
+    }
+
+    return result + 13;
+}
+
